320722f2b6c3d08750374d7d60b388ed58f2b66e
Duy Nguyen <kddesigns90@gmail.com>
2023-09-25 17:05:32 -0500

    add new file

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Duy Nguyen";
+		int age = 33;
+		String state = "Texas";
+		System.out.print("Name: " + name + " Age: " + age + " State: " + state);
+	}
+}
