aa15a396c472a260f07de0a56e0460ddd45aed92
Duy Nguyen <kddesigns90@gmail.com>
2023-09-25 22:59:04 -0500

    add new file

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,10 @@
+public class Objective3Lab3 {
+	public static void main(String[] args) {
+		int First = 1 + (int)(Math.random() * 6);
+		int Second = 1 + (int)(Math.random() * 6);
+		int Third = 1 + (int)(Math.random() * 6);
+		int sum = First+Second+Third;
+		System.out.println("First: " + First + " + " + " Second: " + Second + " + " + Third + "Third: " + " = " + sum);
+	}
+
+}
