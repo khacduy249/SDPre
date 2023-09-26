bbca567812781552acb02e12378745b713a01509
Duy Nguyen <kddesigns90@gmail.com>
2023-09-25 20:45:09 -0500

    add new file

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		int num1 = 8;
+		int num2 = 13;
+		int num3 = 9;
+		System.out.print("average of 3 number is:" + (num1+num2+num3)/3);
+
+	}
+}
