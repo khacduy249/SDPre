981078d93c976c015480f37bd66898baf3f25d0c
Duy Nguyen <145738306+khacduy249@users.noreply.github.com>
2023-09-25 20:11:18 -0500

    Objective2Lab3.java

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -2,9 +2,9 @@ public class Objective2Lab3 {
 	public static void main(String[] args) {
 		int year = 1990;
 		String month = "September";
-		String weekend = "false";
+		boolean isweekend = false;
 		System.out.println(year);
 		System.out.println(month);
-		System.out.println(weekend);
+		System.out.println(isweekend);
 	}
 }
