065302d252d64790d45bf32ee8ac5f0ac4e9eec1
Duy Nguyen <kddesigns90@gmail.com>
2023-09-25 22:19:09 -0500

    add new file

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,7 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1=8, side2=10, hypotenuse;
+		hypotenuse= Math.sqrt((side1*side1)+(side2*side2));
+	System.out.println("The hypotenuse of a triangle is: " +hypotenuse);
+	}
+}
