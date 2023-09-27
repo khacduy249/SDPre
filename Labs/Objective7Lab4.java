899accb32691d62d69cf3956b1c940cbd59109c4
Duy Nguyen <kddesigns90@gmail.com>
2023-09-27 07:57:49 -0500

    Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+
+		int sum = 0;
+		int count = 0;
+		while (count <= 20){
+			sum = sum + count;
+			count++;
+			System.out.println(sum);
+		}
+	}
+}
