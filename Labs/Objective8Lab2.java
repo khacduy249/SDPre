209c5f07e8532fc563ef48567b4f65ee272becb1
Duy Nguyen <kddesigns90@gmail.com>
2023-09-27 15:14:55 -0500

    Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+
+	int sum=0;
+
+		for(int count=1; count<= 20; count++) {  	
+			sum = sum + count;
+		}
+
+		System.out.print(sum);
+	}
+}	
