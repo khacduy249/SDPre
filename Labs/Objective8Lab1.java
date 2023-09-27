10f9737c784b057c71a026b6cc1cdf4bc2e3efb1
Duy Nguyen <kddesigns90@gmail.com>
2023-09-27 12:20:10 -0500

    Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+	public static void main(String[] arge) {
+
+	for (int i=0; i<=10; i+=1) {
+	System.out.println(i);		
+	}	
+
+	for (int i=10; i>0; i-=1) {
+	System.out.println(i);
+	}
+
+	}
+}
